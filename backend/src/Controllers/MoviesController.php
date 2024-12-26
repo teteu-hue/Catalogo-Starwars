a22461c42d0c4be2deaac61367fa27d5400d35fa
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-12-26 16:45:46 -0300

    feat: adicionado rota para /movies, retornando a resposta dos filmes

---
+++ backend/src/Controllers/MoviesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controller;
+use App\Services\ApiStarWars;
+
+class MoviesController extends Controller
+{
+    public function index()
+    {
+        return $this->sendResponse(ApiStarWars::get("https://swapi.py4e.com/api/films"), 200);
+    }
+}
--- backend/src/Routes/index.php
+++ backend/src/Routes/index.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Controllers\HomeController;
+use App\Controllers\MoviesController;
 use App\Router;
 
 $route = new Router();
 
 $route->get('/', HomeController::class, 'index');
 $route->get('/test', HomeController::class, 'test');
+$route->get('/movies', MoviesController::class, 'index');
 
 $route->dispatch();
