2b41a137efedd1e541044b2b98c2c1acbd526cb6
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-12-26 10:47:51 -0300

    classe de Logs da aplicacao

--- src/Models/Logs.php
+++ src/Models/Logs.php
@@ -6,18 +6,19 @@ use App\Database\Dao;
 
 class Logs extends Dao
 {
-    public static function registerLog($params)
+    public function registerLog($params)
     {
-        $sql = "INSERT INTO api_logs(request_time, endpoint, request_method, response_status)
-                VALUES (:request_time, :endpoint, :request_method, :response_status)";
+        $sql = "INSERT INTO api_logs(request_time, endpoint, request_method, response_status, ip_address)
+                VALUES (:request_time, :endpoint, :request_method, :response_status, :ip_address)";
 
         $body = [
             ":request_time" => $params["request_time"],
             ":endpoint" => $params["endpoint"],
             ":request_method" => $params["request_method"],
-            ":response_status" => $params["response_status"]
+            ":response_status" => $params["response_status"],
+            ":ip_address" => $params["ip_address"]
         ];
 
-        self::insertQuery($sql, $body);
+        $this->insertQuery($sql, $body);
     }
 }
