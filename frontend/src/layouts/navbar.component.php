faaf690f402dad76a36473284c6c7f415f6e2846
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-12-28 20:46:24 -0300

    adicionado navegacao entre as paginas

--- frontend/src/layouts/navbar.component.php
+++ frontend/src/layouts/navbar.component.php
@@ -1,5 +1,5 @@
 <nav class="navbar bg-black navbar-expand-lg">
-    <a class="navbar-brand text-white ms-auto" href="#">Catalogo de Filmes</a>
+    <a class="navbar-brand text-white ms-auto" href="./home.view.php">Catalogo de Filmes</a>
     <div class="container-fluid w-auto d-flex">
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,10 +8,10 @@
         <div class="collapse navbar-collapse ms-auto" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item ms-5">
-                    <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active text-white" aria-current="page" href="./home.view.php">Home</a>
                 </li>
                 <li class="nav-item ms-5">
-                    <a class="nav-link text-white" href="#">Filmes</a>
+                    <a class="nav-link text-white" href="./filmes.php">Filmes</a>
                 </li>
             </ul>
         </div>
