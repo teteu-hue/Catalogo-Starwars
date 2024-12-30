e1c4bc9eb8f1a9aa2e92faea6f2c35596da568ad
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-12-30 20:02:26 -0300

    adicionado arquivo para configuracoes globais da aplicacao

--- backend/src/Services/MoviesService.php
+++ backend/src/Services/MoviesService.php
@@ -8,7 +8,7 @@ class MoviesService
 {
     public static function getAll()
     {
-        $response = ApiStarWarsService::get("https://swapi.py4e.com/api/films");
+        $response = ApiStarWarsService::get(BASE_URL . "films");
         $data = ApiStarWarsResource::formatResponse($response);
         return json_decode($data, true);
     }
