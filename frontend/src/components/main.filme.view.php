d30aa2e552ccc7c9fd758f6c93096bb68653816e
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2025-01-05 00:33:58 -0300

    separado os dois modais da aplicacao

--- frontend/src/components/main.filme.view.php
+++ frontend/src/components/main.filme.view.php
@@ -15,54 +15,11 @@
     }
 </style>
 <h1 class="text-center mt-5 pt-5 text-white">Catalogo de Filmes</h1>
-<main id="movies-container" class="w-auto h-auto d-flex align-items-center flex-wrap m-5">
+<main id="movies-container" class="w-auto h-auto d-flex align-items-center flex-wrap justify-content-between">
 </main>
 
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <h1 class="modal-episode fs-5 p-2" id="exampleModalLabel"><span id="title-movie"></span></h1>
-            <div class="modal-header">
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
-            </div>
-            <div class="modal-body">
-                <ul>
-                    <li>
-                        <strong>Título:</strong> <span id="movie-title"></span>
-                    </li>
-                    <li>
-                        <strong>Data de lancamento:</strong> <span id="movie-date"></span>
-                    </li>
-                    <li>
-                        <strong>Idade do filme em anos:</strong> <span id="movie-years"></span>
-                        <br><strong>Idade do filme em meses:</strong> <span id="movie-months"></span>
-                        <br><strong>Idade do filme em dias:</strong> <span id="movie-days"></span>
-                    </li>
-                    <li>
-                        <strong>Número do Episódio:</strong> <span id="movie-episode"></span>
-                    </li>
-                    <li>
-                        <strong>Diretor:</strong> <span id="movie-director"></span>
-                    </li>
-                    <li>
-                        <strong>Produtores:</strong> <span id="movie-producer"></span>
-                    </li>
-                    <li>
-                        <strong>Personagens</strong> <span id="movie-character"></span>
-                    </li>
-                    <li>
-                        <strong>Sinopse:</strong> <span id="movie-opening"></span>
-                    </li>
-                </ul>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Fechar detalhes</button>
-            </div>
-        </div>
-    </div>
-</div>
-</div>
-
+<?= include_once __DIR__ . "/../layouts/modalDetails.component.php" ?>
+<?= include_once __DIR__ . "/../layouts/modalComments.component.php" ?>
 
 <script>
     $(document).ready(function() {
@@ -70,7 +27,7 @@
             url: 'http://localhost:8081/movies',
             method: 'GET',
             dataType: 'json',
-            beforeSend: function(){
+            beforeSend: function() {
                 const loading = `<div class="c-loader m-auto mt-5 justify-content-center"></div>`;
                 const loadingText = `<h2 id="text-loader" class="text-center text-white mt-5 pt-5">Estamos carregando os filmes do catalogo</h2>`;
 
@@ -102,14 +59,14 @@
                     } = movie;
 
                     const characterList = characters
-                    .map(charName => "<a href='#' class='text-decoration-none'>" + charName + "</a>")
-                    .join(", ");
-                   
+                        .map(charName => "<a href='#' class='text-decoration-none'>" + charName + "</a>")
+                        .join(", ");
+
 
                     const card = `
-                <div class="card m-5" style="width: 20rem;">
-                    <div class="card-body bg-danger">
-                        <h5 class="card-title text-white">${title}</h5>
+                <div class="card mx-auto d-flex m-5" style="width: 40rem;">
+                    <div class="card-body " style="background-color:#8C0B23;">
+                        <h5 class="card-title text-white font-bold h1">${title}</h5>
                         <p class="card-text text-white">
                             ${opening_crawl}
                         </p>
@@ -127,7 +84,12 @@
                             data-character="${characterList}"
                             data-bs-toggle="modal" 
                             data-bs-target="#exampleModal" 
-                            class="btn btn-primary text-white">Detalhes...</a>
+                            class="btn btn-primary text-white">Detalhes</a>
+                        <a href="#" 
+                            id="comments"
+                            data-bs-toggle="modal" 
+                            data-bs-target="#commentsModal" 
+                            class="btn btn-success text-white">Comente a respeito desse filme</a>
                     </div>
                 </div>`;
                     $('#movies-container').append(card);
