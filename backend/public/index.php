41d1f6d1495c27d02a81f923d1810295e4ec68b9
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2025-01-13 11:21:49 -0300

    adicionado a timezone de saopaulo para todo metodo de data

--- backend/public/index.php
+++ backend/public/index.php
@@ -3,5 +3,6 @@
 require '../vendor/autoload.php';
 
 require_once(__DIR__ . "/../src/config/app.php");
+require_once(__DIR__ . "/../src/config/timezone.php");
 
 $route = require '../src/Routes/index.php';
